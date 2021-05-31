b51265a1fb011bddb9a0273afe4525c03a34d555
Vinicius Reis <vinicius.moreira2003@gmail.com>
2021-05-31 00:04:53 -0300

    Finalizando ignews!

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import { getPrismicClient } from '../../services/prismic';
 import { RichText } from 'prismic-dom';
 import Prismic from '@prismicio/client';
+import Link from 'next/link';
 
 import styles from './styles.module.scss';
 
@@ -27,11 +28,13 @@ export default function posts({ posts }: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map((post) => (
-            <a key={post.slug} href="#">
-              <time>{post.updatedAt}</time>
-              <strong>{post.title}</strong>
-              <p>{post.excerpt}</p>
-            </a>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.excerpt}</p>
+              </a>
+            </Link>
           ))}
         </div>
       </main>
